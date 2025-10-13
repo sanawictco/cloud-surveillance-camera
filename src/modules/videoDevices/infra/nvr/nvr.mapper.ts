a2b06f437e126aca4e61aaae79e0e00a33e01fca
majid kakavandi <majidkakavandi12776@gmail.com>
2025-10-13 08:28:05 +0330

    feat(videoDevices): add infra layer of videoDevices module

---
+++ src/modules/videoDevices/infra/nvr/nvr.mapper.ts
@@ -0,0 +1,97 @@
+import { Mapper } from 'src/dddLib/infra';
+import { Injectable } from '@nestjs/common';
+
+import { RunningConfigs } from 'src/modules/shared/valueObjects/runningConfigs.vo';
+import { BusinessId } from 'src/dddLib/core/businessId.vo';
+import { Name } from 'src/modules/shared/valueObjects/name.vo';
+import { NvrResponseDto } from '../../contracts/nvr/http/nvr.response.dto';
+import { NvrEntity } from '../../domain/nvr/nvr.entity';
+import { AccessToken } from '../../domain/nvr/valueObjects/accessToken.vo';
+import { CloudIsRecovering } from '../../domain/nvr/valueObjects/cloudIsRecovering.vo';
+import { NvrLanguage } from '../../domain/nvr/valueObjects/NvrLanguage.vo';
+import { NvrPassword } from '../../domain/nvr/valueObjects/nvrPassword.vo';
+import { IsActive } from '../../shared/valueObjects/isActive.vo';
+import { LiveSignalStatus } from '../../shared/valueObjects/liveSignalStatus.vo';
+import { SerialNumber } from '../../shared/valueObjects/serialNumber.vo';
+import { NvrModel } from './nvr.schema';
+import { MaxCameras } from '../../domain/nvr/valueObjects/maxCameras.vo';
+
+@Injectable()
+export class NvrMapper implements Mapper<NvrEntity, NvrModel, NvrResponseDto> {
+  toPersistence(entity: NvrEntity): NvrModel {
+    const copy = entity.getProps();
+    const record: NvrModel = {
+      id: copy.id,
+      name: copy.name,
+      workstationId: copy.workstationId,
+      maxCameras: copy.maxCameras,
+      serialNumber: copy.serialNumber,
+      accessToken: copy.accessToken,
+      password: copy.password,
+      lang: copy.lang,
+      isActive: copy.isActive,
+      liveSignalStatus: copy.liveSignalStatus,
+      cloudIsRecovering: copy.cloudIsRecovering,
+      runningConfigs: copy.runningConfigs,
+      createdAt: copy.createdAt,
+      updatedAt: copy.updatedAt,
+    };
+    return record;
+  }
+
+  toDomain(record: NvrModel): NvrEntity {
+    const entity = new NvrEntity({
+      id: record.id,
+      createdAt: new Date(record.createdAt),
+      updatedAt: new Date(record.updatedAt),
+      props: {
+        name: new Name(record.name),
+        workstationId: new BusinessId(record.workstationId),
+        maxCameras: new MaxCameras(record.maxCameras),
+        serialNumber: new SerialNumber(record.serialNumber),
+        accessToken: new AccessToken(record.accessToken),
+        password: new NvrPassword(record.password),
+        lang: new NvrLanguage(record.lang),
+        isActive: new IsActive(record.isActive),
+        liveSignalStatus: new LiveSignalStatus(record.liveSignalStatus),
+        cloudIsRecovering: new CloudIsRecovering(record.cloudIsRecovering),
+        runningConfigs: new RunningConfigs(record.runningConfigs),
+      },
+    });
+    return entity;
+  }
+
+  toResponse(entity: NvrEntity): NvrResponseDto {
+    const props = entity.getProps();
+    const response = new NvrResponseDto(entity);
+    response.name = props.name;
+    response.workstationId = props.workstationId;
+    response.serialNumber = props.serialNumber;
+    response.password = props.password;
+    response.lang = props.lang;
+    response.isActive = props.isActive;
+    response.liveSignalStatus = props.liveSignalStatus;
+    response.cloudIsRecovering = props.cloudIsRecovering;
+
+    return response;
+  }
+
+  toResponseAll(entities: NvrEntity[]): NvrResponseDto[] {
+    const responseArr: NvrResponseDto[] = [];
+    for (const entity of entities) {
+      const props = entity.getProps();
+      const response = new NvrResponseDto(entity);
+      response.name = props.name;
+      response.workstationId = props.workstationId;
+      response.serialNumber = props.serialNumber;
+      response.password = props.password;
+      response.lang = props.lang;
+      response.isActive = props.isActive;
+      response.liveSignalStatus = props.liveSignalStatus;
+      response.cloudIsRecovering = props.cloudIsRecovering;
+
+      responseArr.push(response);
+    }
+    return responseArr;
+  }
+}
---
+++ src/modules/videoDevices/infra/nvr/nvr.repository.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CacheService } from 'src/extensions/caching/cache.service';
+import { ServiceProvider } from 'src/extensions/serviceProvider/serviceProvider.service';
+import { ParentRepository } from 'src/modules/shared/parent.repository';
+import { RunningConfigs } from 'src/modules/shared/valueObjects/runningConfigs.vo';
+import { NvrResponseDto } from '../../contracts/nvr/http/nvr.response.dto';
+import { NvrValueObjects } from '../../domain/nvr/nvr.type';
+import { NvrEntity } from '../../domain/nvr/nvr.entity';
+import { NvrModel } from './nvr.schema';
+import { NvrMapper } from './nvr.mapper';
+
+@Injectable()
+export class NvrRepository extends ParentRepository<
+  NvrModel,
+  NvrValueObjects,
+  NvrEntity,
+  NvrResponseDto
+> {
+  constructor(
+    @InjectModel(NvrModel.name)
+    protected readonly nvrModel: Model<NvrModel>,
+    protected readonly mapper: NvrMapper,
+    protected readonly cache: CacheService<NvrModel>,
+    protected readonly serviceProvider: ServiceProvider,
+  ) {
+    super(nvrModel, NvrModel, mapper, cache, serviceProvider);
+  }
+
+  async restoreAndInitRecordsToCache(): Promise<void> {
+    const nvrs = await this.nvrModel.find().lean();
+    for (const nvr of nvrs) {
+      nvr.runningConfigs = RunningConfigs.init().unpack();
+      await this.nvrModel.updateOne({ id: nvr.id }, nvr);
+      await this.cache.set(`${NvrModel.name}:${nvr.id}`, nvr);
+    }
+  }
+}
